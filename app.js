7dbabaac6f77dfe3bab3647ab6d38c3473c2e41b
Jose Villanueva <lv428694@gmail.com>
2023-03-28 23:02:17 -0600

    Add event to minus button

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const buttonPlus = document.querySelector(".plus-btn");
+const buttonMinus = document.querySelector(".minus-btn");
+
 let amount = 0;
 
 buttonPlus.addEventListener("click", function(e){
     amount++;
     buttonPlus.closest('.quantity').children[1].value = amount;
+});
+
+buttonMinus.addEventListener("click", function(e){
+    currentValue = buttonMinus.closest('.quantity').children[1].value;
+    if(currentValue > 0){
+        amount--;
+    }
+    buttonMinus.closest('.quantity').children[1].value = amount;
 });
