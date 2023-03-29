ceede6583716b99dc049acf72fa9d29ec47d12a6
Jose Villanueva <lv428694@gmail.com>
2023-03-28 22:30:02 -0600

    Update amount

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Cotizador")
+const buttonPlus = document.querySelector(".plus-btn");
+let amount = 0;
+
+buttonPlus.addEventListener("click", function(e){
+    amount++;
+    buttonPlus.closest('.quantity').children[1].value = amount;
+});
