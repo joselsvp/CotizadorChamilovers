7a9c7063ea059a9e4bfb4f30e8ac5054d5e6402f
Jose Villanueva <lv428694@gmail.com>
2023-04-03 22:31:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 const buttonPlus = document.querySelectorAll(".plus-btn");
 const buttonMinus = document.querySelectorAll(".minus-btn");
 const summaryTotal = document.querySelector(".summary-total");
-var totalProducts = 0;
 
-summaryTotal.innerHTML = '33'
-
-let aux = 0;
-let count = 0
+let productsAdd = [];
 
 buttonPlus.forEach(function(button, index) {
     button.addEventListener("click", function(e){
         let totalAddItems = parseInt( button.closest('.quantity').children[1].value) + 1;
         button.closest('.quantity').children[1].value = totalAddItems;
-        totalProducts = totalAddItems
-
-        if(aux != index){
-            let test = 0;
-            test += totalProducts;
-            console.log("test", test)
-
-            aux++
-  
-        }else { 
-            console.log("totalProducts_2", totalProducts)
-
-        }
+        productsAdd[index] = totalAddItems;
+        showTotalBox(productsAdd)
     });
 
 });
 
+function showTotalBox(productsAdd){
+    let totalAddProducts =  productsAdd.reduce((a , b) => {
+        return a + b;
+    }, 0);
+    summaryTotal.innerHTML = totalAddProducts;
+}
 
 buttonMinus.forEach(function(button){
     button.addEventListener("click", function(e){
@@ -36,6 +27,8 @@ buttonMinus.forEach(function(button){
         if(currentValue > 0){
             currentValue--;
         }
+        productsAdd[index] = totalAddItems;
+
         button.closest('.quantity').children[1].value = currentValue;
     });
 });
